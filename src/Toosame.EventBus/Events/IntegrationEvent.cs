2ed8c7d95baa147cc7e609daf5d806b5b661ac3c
旋风小伙 <lhdboy@outlook.com>
2022-11-11 18:20:28 +0800

    Update IntegrationEvent.cs

--- src/Toosame.EventBus/Events/IntegrationEvent.cs
+++ src/Toosame.EventBus/Events/IntegrationEvent.cs
@@ -1,23 +1,26 @@
-﻿using System;
+using System;
+using System.Text.Json.Serialization;
 
-namespace Toosame.EventBus.Events
+namespace Toosame.EventBus.Events;
+
+public record IntegrationEvent
 {
-    public class IntegrationEvent
+    public IntegrationEvent()
     {
-        public IntegrationEvent()
-        {
-            Id = Guid.NewGuid();
-            CreationDate = DateTime.UtcNow;
-        }
-
-        public IntegrationEvent(Guid id, DateTime createDate)
-        {
-            Id = id;
-            CreationDate = createDate;
-        }
-
-        public Guid Id { get; private set; }
-
-        public DateTime CreationDate { get; private set; }
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
     }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createDate)
+    {
+        Id = id;
+        CreationDate = createDate;
+    }
+
+    [JsonInclude]
+    public Guid Id { get; private init; }
+
+    [JsonInclude]
+    public DateTime CreationDate { get; private init; }
 }
